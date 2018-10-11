6ac17fa1c222ceb15fb11c2bad45dd88c7bf83eb
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-11 12:35:54 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -6,7 +6,7 @@ ORDER BY projects.title ASC;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, users.age, pledges.amount  FROM pledges
+"SELECT users.name, users.age, SUM(pledges.amount) FROM pledges
 JOIN users ON pledges.user_id = users.id
 GROUP BY users.name
 ORDER BY users.name;"
