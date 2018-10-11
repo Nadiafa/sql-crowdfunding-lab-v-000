3c12d12208dbc500dbd6873f173230b5c22315c8
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-11 12:41:45 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -13,17 +13,19 @@ ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT FROM"
+"SELECT projects.title, SUM(pledges.amount) AS amoutn_over_goal FROM projects
+JOIN pledges ON projects.funding_goal >= pledges.amount
+GROUP BY projects.title;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT FROM"
+"SELECT"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT FROM"
+"SELECT"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"SELECT FROM"
+"SELECT"
 end
