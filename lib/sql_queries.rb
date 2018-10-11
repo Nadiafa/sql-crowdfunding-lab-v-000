f56fd283a51c3bb9bb3c748772f11cd6366fc459
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-11 12:35:07 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,6 +8,7 @@ end
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "SELECT users.name, users.age, pledges.amount  FROM pledges
 JOIN users ON pledges.user_id = users.id
+GROUP BY users.name
 ORDER BY users.name;"
 end
 
